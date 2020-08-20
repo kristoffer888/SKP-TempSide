7d58a83473a45eb1d809dbd868bf4cae8cfefd1f
kris541k <kristofferclausen@rocketmail.com>
2020-08-20 15:06:09 +0200

    Time "works"

--- SKP-Temp/index.php
+++ SKP-Temp/index.php
@@ -1,29 +1,58 @@
-<?php
+<link rel="stylesheet" href="style.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function(){
+        var jsondata = $.ajax({
+            url: "data.php",
+            dataType: "JSON",
 
-$host="localhost";
-$username="root";
-$password="";
-$db_name="test";
-
-
-$con = mysqli_connect($host, $username, $password, $db_name);
-
-if (!$con)
-{
-    die("Connection failed:" . mysqli_connect_error());
-}
-?>
-
-<?php
-$time = date("Y/m/d", strtotime('last monday'));
-echo $time;
-
-?>
+            success: function (data) {
+                console.log(data)
+            },error:function (error){
+                console.log(error)
+            }
+        });
+    });
+</script>
 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>skp</title>
+
+    <div class="dropdown">
+        <button onclick="myFunction()" class="dropbtn">Zoner</button>
+        <div id="myDropdown" class="dropdown-content">
+            <a href="#contact">Zone5</a>
+            <a href="#contact">Zone6</a>
+            <a href="#contact">Zone8</a>
+            <a href="#contact">Zone9</a>
+            <a href="#contact">Zone100</a>
+            <a href="#contact">Zone102</a>
+        </div>
+    </div>
+
+    <center><h1>Zone 5</h1></center>
+    <script>
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+    </script>
+
+
     <script
             type="text/javascript"
             src="https://www.gstatic.com/charts/loader.js"
@@ -36,12 +65,15 @@ echo $time;
                 ['Tid', 'Temperatur °C', {role: 'annotation'}, 'Luftfugtighed %', {role: 'annotation'}],
 
                 <?php
+                $conn = mysqli_connect("localhost", "root", "", "test");
+                $result = mysqli_query($conn, "SELECT humidity, zone, temperature, updated FROM climatesensor WHERE zone = 8 and ((updated > '2020-08-12 08:00:00' and updated < '2020-08-12 08:05:00') or (updated > '2020-08-12 12:00:00' and updated < '2020-08-12 12:05:00') or (updated > '2020-08-12 15:00:00' AND updated < '2020-08-12 15:05:00'))");
 
-                $sql_tabel = "SELECT * FROM ( SELECT * FROM climateSensor WHERE zone = 5 ORDER BY id DESC LIMIT 3 ) AS r ORDER BY id";
-                $resultat = mysqli_query($con, $sql_tabel);
 
-                while ($row = mysqli_fetch_assoc($resultat)){
-                $Tid = $row['updated'];
+                while ($row = mysqli_fetch_assoc($result)){
+                $dateTime = $row['updated'];
+                $dateTime = date_create($dateTime);
+                $Date = date_format($dateTime, "Y-m-d");
+                $Tid = date_format($dateTime, "H:i:s");
                 $Temperature = $row['temperature'];
                 $Humidity = $row['humidity'];
                 ?>
@@ -54,7 +86,7 @@ echo $time;
 
             var optionsMon = {
                 colors: ['orange','blue'],
-                title: 'Man',
+                title: "Mandag <?php echo ($Date) ?>",
                 vAxis: {minValue: 0},
                 series: {
                     0: {
@@ -95,6 +127,6 @@ echo $time;
     </script>
 </head>
 <body>
-<div id="chart_Mon" style="width: 100%; height: 70%"></div>
+<div id="chart_Mon" style="width: 50% !important; height: 100% !important;"></div>
 </body>
 </html>
