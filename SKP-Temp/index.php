4cf50b692184d932a46e3846238b4fd4e222eba5
fred460f <frederik@rovi.dk>
2020-08-27 14:53:55 +0200

    Made graphs pretty :)

--- SKP-Temp/index.php
+++ SKP-Temp/index.php
@@ -8,6 +8,7 @@
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <script src="week-picker.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
@@ -156,7 +157,6 @@
 
 
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <script>
         function drawChart() {
             $('#chart0').remove();
@@ -164,11 +164,16 @@
             $('#chart2').remove();
             $('#chart3').remove();
             $('#chart4').remove();
-            $('#chartContainer').append('<canvas id="chart0" height="30%" width="100%"></canvas>');
-            $('#chartContainer').append('<canvas id="chart1" height="30%" width="100%"></canvas>');
-            $('#chartContainer').append('<canvas id="chart2" height="30%" width="100%"></canvas>');
-            $('#chartContainer').append('<canvas id="chart3" height="30%" width="100%"></canvas>');
-            $('#chartContainer').append('<canvas id="chart4" height="30%" width="100%"></canvas>');
+            $('#padding0').remove();
+            $('#padding1').remove();
+            $('#padding2').remove();
+            $('#padding3').remove();
+            $('#padding4').remove();
+            $('#chartContainer').append('<canvas id="chart0" height="30%" width="100%"></canvas> <div id="padding0" style="padding-bottom: 50px"></div>');
+            $('#chartContainer').append('<canvas id="chart1" height="30%" width="100%"></canvas> <div id="padding1" style="padding-bottom: 50px"></div>');
+            $('#chartContainer').append('<canvas id="chart2" height="30%" width="100%"></canvas> <div id="padding2" style="padding-bottom: 50px"></div>');
+            $('#chartContainer').append('<canvas id="chart3" height="30%" width="100%"></canvas> <div id="padding3" style="padding-bottom: 50px"></div>');
+            $('#chartContainer').append('<canvas id="chart4" height="30%" width="100%"></canvas> <div id="padding4" style="padding-bottom: 50px"></div>');
 
             var ctx = document.getElementById('chart0').getContext('2d');
             var chart0 = new Chart(ctx, {
@@ -338,7 +343,7 @@
                         fill: true,
                     }, {
                         label: 'Luftfugtighed',
-                        backgroundColor: 'rgba31,84,208,0.1)',
+                        backgroundColor: 'rgba(31,84,208,0.1)',
                         borderColor: 'rgb(31,84,208)',
                         data: [listList[3][0].humidity, listList[3][1].humidity, listList[3][2].humidity],
                         fill: true,
