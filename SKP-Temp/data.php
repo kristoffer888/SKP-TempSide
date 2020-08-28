849fc1dc8b6963f478be4ac959972c8a63e0e49a
fred460f <fred460f@edu.sde.dk>
2020-08-28 08:34:24 +0200

    Made graphs pretty :)

--- SKP-Temp/data.php
+++ SKP-Temp/data.php
@@ -2,12 +2,12 @@
 $conn = mysqli_connect("localhost", "root", "", "test");
 $result = mysqli_query($conn, "SELECT humidity, zone, temperature, updated FROM climatesensor WHERE ((time(updated) > time('08:00') and time(updated) < '08:05') or (time(updated) > '12:00' and time(updated) < '12:05') or (time(updated) > '15:00' AND time(updated) < '15:05'))");
 
-$data =  array();
+$data = array();
 
 while ($row = $result->fetch_assoc()) {
     $data[] = $row;
 }
 
-print_r( json_encode($data));
+print_r(json_encode($data));
 $conn->close();
 exit();
