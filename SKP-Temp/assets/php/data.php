4ee87cd872b016c3f0c5f83dccfb305c26116609
kristoffer8881 <kristofferclausen@rocketmail.com>
2021-04-26 12:44:07 +0200

    fixes

--- SKP-Temp/assets/php/data.php
+++ SKP-Temp/assets/php/data.php
@@ -1,6 +1,9 @@
 <?php
-$conn = mysqli_connect("localhost", "infotavle.itd-sk", "OVSZY0Jt", "infotavle_itd_skp_sde_dk");
-$result = mysqli_query($conn, "SELECT humidity, zone, temperature, updated FROM climateSensor WHERE ((time(updated) > time('08:00') and time(updated) < '08:05') or (time(updated) > '12:00' and time(updated) < '12:05') or (time(updated) > '15:00' AND time(updated) < '15:05'))");
+$conn = mysqli_connect("localhost", "root", "", "skptemp");
+$arr = $_GET['week'];
+ //$arr = ['2019-12-16', '2019-12-17', '2019-12-18', '2019-12-19', '2019-12-20'];
+
+$result = mysqli_query($conn, "SELECT humidity, zone, temperature, updated FROM climatesensor WHERE zone = 5 AND (date(updated) = '$arr[0]' OR date(updated) = '$arr[1]' OR date(updated) = '$arr[2]' OR date(updated) = '$arr[3]' OR date(updated) = '$arr[4]') AND ((time(updated) > time('08:00') and time(updated) < '08:05') or (time(updated) > '12:00' and time(updated) < '12:05') or (time(updated) > '15:00' AND time(updated) < '15:05'))");
 
 $data = array();
 
